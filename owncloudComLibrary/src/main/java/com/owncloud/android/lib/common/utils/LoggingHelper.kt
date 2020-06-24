09a773f3668d17670daa1e4b2dac3b0c5559b9ab
Hannes Achleitner <hannes.software@gmx.at>
2020-06-24 12:25:08 +0200

    Update logcat for Crashlytic

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/utils/LoggingHelper.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/utils/LoggingHelper.kt
@@ -1,5 +1,8 @@
 package com.owncloud.android.lib.common.utils
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import info.hannes.crashlytic.CrashlyticsTree
+import info.hannes.logcat.BuildConfig
 import info.hannes.timber.DebugTree
 import info.hannes.timber.FileLoggingTree
 import info.hannes.timber.fileLoggingTree
@@ -16,6 +19,10 @@ object LoggingHelper {
             directory.mkdirs()
         Timber.plant(FileLoggingTree(directory, filename = storagePath))
         Timber.plant(DebugTree())
+        Timber.plant(CrashlyticsTree())
+
+        FirebaseCrashlytics.getInstance().setCustomKey("VERSION_NAME", BuildConfig.VERSIONNAME)
+        Timber.plant(CrashlyticsTree())
     }
 
     fun stopLogging() {
